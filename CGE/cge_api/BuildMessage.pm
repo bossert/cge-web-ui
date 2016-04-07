5ff2f0cc506206191b10a5047b2273004bda02c3
Aaron Bossert <bossert@cray.com>
2016-04-07 12:15:12 -0500

    incremental update to all files

--- CGE/cge_api/BuildMessage.pm
+++ CGE/cge_api/BuildMessage.pm
@@ -5,9 +5,10 @@ package CGE::cge_api::BuildMessage;
 use strict;
 use 5.016;
 use Carp qw(croak cluck carp);
-use Rpn qw(compile2rpn);
-use Constants qw(:ALL);
-
+use lib '/mnt/lustre/bossert/git';
+use CGE::cge_api::Rpn qw(compile2rpn);
+use CGE::cge_api::Constants qw(:ALL);
+use Data::Dumper;
 require Exporter;
 
 our $VERSION = 0.01;
@@ -16,8 +17,78 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw();
 our %EXPORT_TAGS = (ALL => [ qw() ]);
 
+#=+ Here for private and exportable subs, apart from those added to EXPORT_OK, we also use the underscore convention:
+#   any subs that start with an underscore are intended for internal use only.
+
 sub buildMessage {
   my ($messageType) = @_;
+}
+
+sub _buildNvpBytes {
+  my ($nvp_ref) = @_;
+  my $nvp_bytes;
+  
+  foreach my $nvp(@{$nvp_ref}) {
+    #=+ Store the length as an unsigned Long value (32 bits)
+    my $length = pack('L>',length($nvp->{'key'}));
+    #=+ Store the name
+    my $name = pack('A*',$nvp->{'key'});
+    #=+ Store the value as an unsigned Quad (64 bits)
+    my $value = pack('Q>',$nvp->{'value'});
+    $nvp_bytes .= $length.$name.$value;
+  }
+  return $nvp_bytes;
+}
+
+sub _buildHeaderBytes {
+  my ($header_ref) = @_;
+  $header_ref->{'message_length'} = 10;
+  $header_ref->{'message_type'} = 'GET_OUTPUT_DIRECTORY';
+  
+  my $magic = pack('A8',$MAGIC_BYTES_STRING);
+  
+  my $version = pack('b8',sprintf('%b',$CURRENT_VERSION - 1));
+  my $message_type = pack('b8',sprintf('%b',$CGE_MESSAGE_CODES{$header_ref->{'message_type'}}));
+  
+  my $nvps_present;
+  my $nvp_bytes;
+  if(exists $header_ref->{'nvp_list'}) {
+    $nvps_present = pack('b8',sprintf('%b',@{header_ref->{'nvp_list'}}));
+    $nvp_bytes = _buildNvpBytes($header_ref->{'nvp_list'});
+  }
+  else {
+    $nvps_present = pack('b8','00000000');
+  }
+  
+  my $log_config_present;
+  my $log_config_bytes;
+  if (exists $header_ref->{'log_config'}) {
+    $log_config_present = pack('b8','00000001');
+  }
+  else {
+    $log_config_present = pack('b8','00000000');
+  }
+  
+  #=+ The length is the second field, but we need to know how long the message/payload is as well as the entire header,
+  #   So we calculate as the last step before building the header bytes.  Also, there are more compact ways to add up
+  #   the lengths and to concatenate the header bytes together, but the slightly more verbose approach should make it
+  #   easier to read the code and modify later as needed by someone other than the primary author.
+  my $lengthNum = $header_ref->{'message_length'};
+  $lengthNum    += length($version);
+  $lengthNum    += length($message_type);
+  $lengthNum    += length($nvps_present);
+  $lengthNum    += length($nvp_bytes) if defined $nvp_bytes;
+  $lengthNum    += length($log_config_present);
+  $lengthNum    += length($log_config_bytes) if defined $log_config_bytes;
+  
+  my $length = pack('Q>',$lengthNum);
+  
+  my $header_bytes = $magic.$length.$version.$message_type.$nvps_present;
+  $header_bytes    .= $nvp_bytes if defined $nvp_bytes;
+  $header_bytes    .= $log_config_present;
+  $header_bytes    .= $log_config_bytes if defined $log_config_bytes;
+  
+  return $header_bytes;
 }
 
 1;
--- CGE/cge_api/Constants.pm
+++ CGE/cge_api/Constants.pm
@@ -9,67 +9,62 @@ require Exporter;
 our $VERSION = 0.01;
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(%protocol %messageTypes_num2str %messageTypes_str2num %errorCodes_num2str %errorCodes_str2num);
-our %EXPORT_TAGS = (ALL => [ qw(%protocol %messageTypes_num2str %messageTypes_str2num %errorCodes_num2str %errorCodes_str2num) ]);
+our @EXPORT_OK = qw($V1 $V2 $LOWEST_SUPPORTED_VERSION $HIGHEST_SUPPORTED_VERSION $CURRENT_VERSION $MAGIC_BYTES_STRING $SSH_HEADER_STRING $V1_HEADER_LENGTH $V2_MIN_HEADER_LENGTH $V2_MAX_NVPS %CGE_MESSAGE_CODES %CGE_ERROR_CODES);
+our %EXPORT_TAGS = (ALL => [ qw($V1 $V2 $LOWEST_SUPPORTED_VERSION $HIGHEST_SUPPORTED_VERSION $CURRENT_VERSION $MAGIC_BYTES_STRING $SSH_HEADER_STRING $V1_HEADER_LENGTH $V2_MIN_HEADER_LENGTH $V2_MAX_NVPS %CGE_MESSAGE_CODES %CGE_ERROR_CODES) ]);
 
-#=+ These values need to get calculated within the hash to avoid entering them twice
-my $magic_bytes_string = 'SftUrika';
-my $ssh_header_string = 'SSH-2.0-';
-my $v1_header_length = 2;
+our $V1                        = 1;
+our $V2                        = 2;
+our $LOWEST_SUPPORTED_VERSION  = 1;
+our $HIGHEST_SUPPORTED_VERSION = 2;
+our $CURRENT_VERSION           = 2;
+our $MAGIC_BYTES_STRING        = 'SftUrika';
+our $SSH_HEADER_STRING         = 'SSH-2.0-';
+our $V1_HEADER_LENGTH          = 2;
+our $V2_MIN_HEADER_LENGTH      = $V1_HEADER_LENGTH + 2;
+our $V2_MAX_NVPS               = 255;
 
-my %protocol = ('V1'                        => 1,
-                'V2'                        => 2,
-                'LOWEST_SUPPORTED_VERSION'  => 1,
-                'HIGHEST_SUPPORTED_VERSION' => 2,
-                'CURRENT_VERSION'           => 2,
-                'MAGIC_BYTES_STRING'        => $magic_bytes_string,
-                'MAGIC_BYTES'               => unpack('H*',$magic_bytes_string),
-                'MAGIC_BYTES_LENGTH'        => length($magic_bytes_string),
-                'SSH_HEADER_STRING'         => $ssh_header_string,
-                'SSH_HEADER_BYTES'          => unpack('H*',$ssh_header_string),
-                'V1_HEADER_LENGTH'          => $v1_header_length,
-                'V2_MIN_HEADER_LENGTH'      => $v1_header_length + 2,
-                'V2_MAX_NVPS'               => 255);
+our %CGE_MESSAGE_CODES =(0   => 'ERROR',
+                         1   => 'READY_FOR_DATA',
+                         2   => 'SUCCESS',
+                         10  => 'QUERY_REQUEST',
+                         11  => 'QUERY_RESULTS',
+                         20  => 'UPDATE_REQUEST',
+                         30  => 'CHECKPOINT_REQUEST',
+                         200 => 'GET_DEFAULT_NVPS',
+                         201 => 'GET_DEFAULT_LOGGING',
+                         202 => 'GET_OUTPUT_DIRECTORY',
+                         210 => 'SET_DEFAULT_NVPS',
+                         211 => 'SET_DEFAULT_LOGGING',
+                         212 => 'SET_OUTPUT_DIRECTORY',
+                         250 => 'SHUTDOWN_REQUEST',
+                         255 => 'ECHO',
+                         'ERROR'                => 0,
+                         'READY_FOR_DATA'       => 1,
+                         'SUCCESS'              => 2,
+                         'QUERY_REQUEST'        => 10,
+                         'QUERY_RESULTS'        => 11,
+                         'UPDATE_REQUEST'       => 20,
+                         'CHECKPOINT_REQUEST'   => 30,
+                         'GET_DEFAULT_NVPS'     => 200,
+                         'GET_DEFAULT_LOGGING'  => 201,
+                         'GET_OUTPUT_DIRECTORY' => 202,
+                         'SET_DEFAULT_NVPS'     => 210,
+                         'SET_DEFAULT_LOGGING'  => 211,
+                         'SET_OUTPUT_DIRECTORY' => 212,
+                         'SHUTDOWN_REQUEST'     => 250,
+                         'ECHO'                 => 255);
 
-my %messageTypes_num2str = (0   => 'ERROR',
-                            1   => 'READY_FOR_DATA',
-                            2   => 'SUCCESS',
-                            10  => 'QUERY_REQUEST',
-                            11  => 'QUERY_RESULTS',
-                            20  => 'UPDATE_REQUEST',
-                            30  => 'CHECKPOINT_REQUEST',
-                            200 => 'GET_DEFAULT_NVPS',
-                            201 => 'GET_DEFAULT_LOGGING',
-                            202 => 'GET_OUTPUT_DIRECTORY',
-                            210 => 'SET_DEFAULT_NVPS',
-                            211 => 'SET_DEFAULT_LOGGING',
-                            212 => 'SET_OUTPUT_DIRECTORY',
-                            250 => 'SHUTDOWN_REQUEST',
-                            255 => 'ECHO');
-
-#=+ Perhaps this is total overkill, extreme laziness, or both, but the thought process is that in order to avoid
-#   writing the values twice in row, thereby doubling the possibility of typos and time needed to add/alter
-#   message type code mappings at a later date, we write it once and then simply swap them out.  Sure, swapping them in this
-#   way introduces the possibility of having duplicate hash values, but since we are working with string to
-#   numeric mappings (and vice versa) of message codes, by definition, each will be unique
-
-my %messageTypes_str2num;
-
-while (my($k,$v) = each %messageTypes_num2str) {
-  $messageTypes_str2num{$v} = $k;
-}
-
-my %errorCodes_num2str = (1000 => 'BAD_MAGIC_BYTES',
-                          1001 => 'INVALID_DATA',
-                          1010 => 'UNSUPPORTED_PROTOCOL_VERSION',
-                          1011 => 'UNSUPPORTED_PROTOCOL_FEATURE',
-                          1021 => 'UNSUPPORTED_MESSAGE_TYPE',
-                          1022 => 'UNSUPPORTED_MESSAGE_DIRECTION');
-
-my %errorCodes_str2num;
-
-while (my($k,$v) = each %errorCodes_num2str) {
-  $errorCodes_str2num{$v} = $k;
-}
+our %CGE_ERROR_CODES = (1000 => 'BAD_MAGIC_BYTES',
+                        1001 => 'INVALID_DATA',
+                        1010 => 'UNSUPPORTED_PROTOCOL_VERSION',
+                        1011 => 'UNSUPPORTED_PROTOCOL_FEATURE',
+                        1021 => 'UNSUPPORTED_MESSAGE_TYPE',
+                        1022 => 'UNSUPPORTED_MESSAGE_DIRECTION',
+                        BAD_MAGIC_BYTES               => 1000,
+                        INVALID_DATA                  => 1001,
+                        UNSUPPORTED_PROTOCOL_VERSION  => 1010,
+                        UNSUPPORTED_PROTOCOL_FEATURE  => 1011,
+                        UNSUPPORTED_MESSAGE_TYPE      => 1021,
+                        UNSUPPORTED_MESSAGE_DIRECTION => 1022);
 
 1;
